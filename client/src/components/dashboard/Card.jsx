06335f61002705553502b4c1880bfa6f0886223d
José Agustín de la Puente <14jdelap@gmail.com>
2021-10-07 10:42:47 -0500

    Add labels and due dates when appropriate.
    Co-authored-by: Juan Garcia srgarci79@gmail.com
    Co-authored-by: Jose de la Puente 14jdelap@gmail.com

--- client/src/components/dashboard/Card.jsx
+++ client/src/components/dashboard/Card.jsx
@@ -3,25 +3,20 @@ import Label from "./Label"
 
 const Card = ({ card }) => {
 
-  function generateRandomInteger() {
-    // To do: is this correct?
-    return Math.random()*10000000000000000;
-  }
-
   return <div className="card-background">
     <div className="card ">
       <i className="edit-toggle edit-icon sm-icon"></i>
       <div className="card-info">
-        {card.labels.map(label => <Label key={generateRandomInteger()} color={label} />)}
+        {card.labels.map(label => <Label key={label} color={label} />)}
         <p>
           {card.title}
         </p>
       </div>
 
       <div className="card-icons">
-        <i className="clock-icon sm-icon overdue-recent completed">
-          Aug 4
-        </i>
+        { card.dueDate === null ? "" : <i className="clock-icon sm-icon overdue-recent completed">
+          {card.dueDate}
+        </i>}
         <i className="description-icon sm-icon"></i>
         <i className="comment-icon sm-icon"></i>
       </div>
--- client/src/components/dashboard/ExistingCards.jsx
+++ client/src/components/dashboard/ExistingCards.jsx
@@ -8,76 +8,7 @@ const ExistingCards = ({ listId }) => {
 
 
   return <div id="cards-container" data-id="list-1-cards">
-    {filteredCards.map(card => (<Card key={card._id} card={card} />))}
-    <div className="card-background">
-      <div className="card ">
-        <i className="edit-toggle edit-icon sm-icon"></i>
-        <div className="card-info">
-          <div className="card-label green colorblindable"></div>
-          <div className="card-label yellow colorblindable"></div>
-          <div className="card-label red colorblindable"></div>
-          <div className="card-label orange colorblindable"></div>
-          <div className="card-label blue colorblindable"></div>
-          <div className="card-label purple colorblindable"></div>
-          <p>
-            Cards do many cool things. Click on this card to
-            open it and learn more...
-          </p>
-        </div>
-        <div className="card-icons">
-          <i className="clock-icon sm-icon overdue-recent completed">
-            Aug 4
-          </i>
-          <i className="description-icon sm-icon"></i>
-          <i className="comment-icon sm-icon"></i>
-        </div>
-      </div>
-    </div>
-    <div className="card-background">
-      <div className="card ">
-        <i className="edit-toggle edit-icon sm-icon"></i>
-        <div className="cover-image"></div>
-        <div className="card-info">
-          <p>Another list with stuff</p>
-        </div>
-        <div className="card-icons">
-          <i className="clock-icon sm-icon overdue ">Aug 3</i>
-          <i className="description-icon sm-icon"></i>
-        </div>
-      </div>
-    </div>
-    <div className="card-background">
-      <div className="card ">
-        <i className="edit-toggle edit-icon sm-icon"></i>
-        <div className="cover-image"></div>
-        <div className="card-info">
-          <p>
-            Use the + in the top menu to make your first board
-            now.
-          </p>
-        </div>
-        <div className="card-icons"></div>
-      </div>
-    </div>
-  </div>
-  /*const cards = useSelector(state => state.lists)
-
-  console.log(lists)
-  return <div id="list-container" className="list-container">
-    <div id="existing-lists" className="existing-lists">
-      {lists.map(list => (
-        <List key={list._id} list={list} />
-      ))}
-    </div>
-    <div id="new-list" className="new-list">
-            <span>Add a list...</span>
-            <input type="text" placeholder="Add a list..." />
-            <div>
-              <input type="submit" className="button" value="Save" />
-              <i className="x-icon icon"></i>
-            </div>
-          </div>
-  </div>*/
+    {filteredCards.map(card => (<Card key={card._id} card={card} />))}</div>
 }
 
 export default ExistingCards
